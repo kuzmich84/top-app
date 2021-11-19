929fe76577e6892214c6dabfe97ab10086db8356
Kuchukov Dmitry <kudim@inbox.ru>
2021-11-19 16:06:30 +0300

    Подготовка проекта

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>MyTop - лучший топ</title>
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap"
+                rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
