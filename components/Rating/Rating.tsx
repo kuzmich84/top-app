3444a54e775de2f9c35c837507202f040f288f1d
dmitry.kuchukov <kudim84@gmail.com>
2021-11-20 17:16:12 +0300

    Добавит в Rating возможность устанавливать рейтинг

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,25 +1,50 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {RatingProps} from "./rating.props"
 import StarIcon from "./star.svg"
+import styles from "./rating.module.css"
+import cn from "classnames"
 
 
 export function Rating({isEditable = false, rating, setRating, ...props}: RatingProps): JSX.Element {
 
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>))
 
+    const changeDisplay = (i: number) => {
+        if (!isEditable) return
+        constructRating(i)
+    }
+
+    const handlerClick = (i: number) => {
+        if (!isEditable || !setRating) return
+        setRating(i)
+    }
+
     const constructRating = (currentRating: number) => {
-        const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
+        const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (<StarIcon
-            class
+                className={cn(styles.star, {
+                    [styles.filled]: i < currentRating,
+                    [styles.editable]: isEditable
+
+                })}
+                onMouseEnter={() => changeDisplay(i + 1)}
+                onMouseLeave={() => changeDisplay(rating)}
+                onClick={() => handlerClick(i + 1)}
             />)
         })
+
+        setRatingArray(updatedArray)
     }
+
+    useEffect(() => {
+        constructRating(rating)
+    }, [rating])
 
 
     return (
         <div {...props}>
-
+            {ratingArray.map((r, i) => <span key={i}>{r}</span>)}
         </div>
-    );
+    )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,23 @@
 import type {NextPage} from 'next'
-import {Button, Htag, P, Tag} from '../components/'
-import {useEffect, useState} from "react"
+import {Button, Htag, P, Rating, Tag} from '../components/'
+import {useState} from "react"
 
 
 const Home: NextPage = (): JSX.Element => {
 
-    const [counter, setCounter] = useState<number>(0)
-    useEffect(()=>{
-        console.log(`counter`, counter)
-        return function cleanup(){
-            console.log(`Unmount`, counter)
-        }
-    },[counter])
+    const [rating, setRating] = useState<number>(4)
 
     return (
         <div>
-            <Htag>{counter}</Htag>
-            <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
+            <Htag tag='h1'>Заголовок</Htag>
+            <Button appearance='primary' arrow='right'>Кнопка</Button>
             <Button appearance='ghost' arrow="down">Кнопка</Button>
             <P size="s">Кучуков</P>
             <P size="l">Дмитрий</P>
             <Tag color='green' size="s">+79215777600</Tag>
             <Tag color='red' size="m">+79215777600</Tag>
             <Tag size="s">+79215777600</Tag>
+            <Rating rating={rating} isEditable={true} setRating={setRating} />
         </div>
     )
 }
