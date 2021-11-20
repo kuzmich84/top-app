263459affa95dacbbc6c660e74d763e2449cfe46
dmitry.kuchukov <kudim84@gmail.com>
2021-11-20 16:31:26 +0300

    добавит svg star

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
 import type {NextPage} from 'next'
-import {Button, Htag, P} from '../components/';
+import {Button, Htag, P, Tag} from '../components/'
+import {useEffect, useState} from "react"
 
 
 const Home: NextPage = (): JSX.Element => {
+
+    const [counter, setCounter] = useState<number>(0)
+    useEffect(()=>{
+        console.log(`counter`, counter)
+        return function cleanup(){
+            console.log(`Unmount`, counter)
+        }
+    },[counter])
+
     return (
         <div>
-            <Htag>Текст</Htag>
-            <Button appearance='primary' arrow='right'>Кнопка</Button>
+            <Htag>{counter}</Htag>
+            <Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
             <Button appearance='ghost' arrow="down">Кнопка</Button>
-            <P fontSize="18">Дмитрий</P>
+            <P size="s">Кучуков</P>
+            <P size="l">Дмитрий</P>
+            <Tag color='green' size="s">+79215777600</Tag>
+            <Tag color='red' size="m">+79215777600</Tag>
+            <Tag size="s">+79215777600</Tag>
         </div>
     )
 }
