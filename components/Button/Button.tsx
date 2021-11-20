c8a76626f0fea97edd8f7ee7062f761fec81b612
dmitry.kuchukov <kudim84@gmail.com>
2021-11-20 13:30:22 +0300

    Создает Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import {ButtonProps} from "./Button.props"
 import styles from "./Button.module.css"
 import cn from "classnames"
+import ArrowIcon from "./arrow.svg"
 
-export const Button = ({appearance, children}: ButtonProps): JSX.Element => {
+export const Button = ({appearance, children, className, arrow = 'none', ...props}: ButtonProps): JSX.Element => {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, className, {
                 [styles.primary]: appearance == 'primary',
                 [styles.ghost]: appearance == 'ghost'
             })}
+            {...props}
         >{children}
+            {arrow !== 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow === 'down'
+            })}>
+                <ArrowIcon />
+            </span>}
         </button>
     )
 }
