a7068ffcb3c2b8132054d2aea1f51ba1a293f01b
dmitry.kuchukov <kudim84@gmail.com>
2021-11-20 16:30:07 +0300

    Создает компонент P

--- components/P/P.tsx
+++ components/P/P.tsx
@@ -3,11 +3,14 @@ import {PProps} from "./p.props";
 import styles from "./p.module.css"
 import cn from "classnames";
 
-export function P({children, fontSize, ...props}: PProps) {
+export function P({children, size, className, ...props}: PProps) {
     return (
         <p
-            className={cn(styles.p)}
-            style={{fontSize:`${fontSize}px`}}
+            className={cn(styles.p, className, {
+                [styles.s]: size === 's',
+                [styles.m]: size === 'm',
+                [styles.l]: size === 'l'
+            })}
             {...props}
         >{children}
         </p>
