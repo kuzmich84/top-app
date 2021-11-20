d834c4c1809ba8b3268657cc40fb04788a2e468d
dmitry.kuchukov <kudim84@gmail.com>
2021-11-20 13:31:53 +0300

    Добавит svg icon arrow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    images: {
+        domains: ['courses-top.ru']
+    },
+    webpack(config, options) {
+        config.module.rules.push({
+            loader: '@svgr/webpack',
+            options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                    plugins: [{ removeViewBox: false }],
+                },
+                titleProp: true,
+            },
+            test: /\.svg$/,
+        });
+
+        return config;
+    },
 }
